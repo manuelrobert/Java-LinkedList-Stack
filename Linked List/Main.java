88396417240a8a3a0495aba8bbf1fc7d323adda7
Manuel Robert <manuelrobz@outlook.com>
2021-05-04 12:03:42 +0530

    Linked List almost completed

--- Linked List/Main.java
+++ Linked List/Main.java
@@ -22,10 +22,41 @@ class LinkedList {
         }
     }
 
+    public void insertAt(int index, int data) {
+        Node n = new Node();
+        n.data = data;
+        n.next = null;
+        if (index == 0) {
+            n.next = head;
+            head = n;
+        } else {
+            Node node = head;
+            for (int i = 0; i < index - 1; i++) {
+                node = node.next;
+            }
+            n.next = node.next;
+            node.next = n;
+        }
+    }
+
+    public void deleteAt(int index) {
+        if (index == 0) {
+            head = head.next;
+        } else {
+            Node node = head;
+            for (int i = 0; i < index - 1; i++) {
+                node = node.next;
+            }
+            Node t = node.next;
+            node.next = t.next;
+            t = null;
+        }
+    }
+
     public void traverse() {
-        if(head == null){
+        if (head == null) {
             System.out.println("Linked List empty");
-        }else{
+        } else {
             Node node = head;
             while (node.next != null) {
                 System.out.println(node.data);
@@ -39,9 +70,18 @@ class LinkedList {
 public class Main {
     public static void main(String[] args) {
         LinkedList l = new LinkedList();
-        l.insert(10);
-        l.insert(20);
+        l.traverse();
         l.insert(30);
+        l.insert(40);
+        l.insert(50);
+        l.traverse();
+        l.insertAt(0, 10);
+        l.traverse();
+        l.insertAt(1, 20);
+        l.traverse();
+        l.deleteAt(0);
+        l.traverse();
+        l.deleteAt(3);
         l.traverse();
     }
 }
